9c8edcb08899b91270ac3ccbc2cafaee7747a777
Matt Bachman <mattbachman31@gmail.com>
2015-08-19 01:01:21 -0400

    Create player.h

---
+++ player.h
@@ -0,0 +1,22 @@
+#ifndef PLAYER
+#define PLAYER
+
+#include <iostream>
+#include <stdio.h>
+#include <string>
+#include <fstream>
+#include <cstdlib>
+
+using namespace std;
+
+class Player{
+public:
+	string name;
+	int buyingPrice;
+	int sellingPrice;
+	int profit;
+};
+
+
+
+#endif
